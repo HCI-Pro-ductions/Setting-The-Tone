0858faf201c2984c16865275a9211963880d0ff1
Beans <106309084+Life-is-unpog@users.noreply.github.com>
2022-06-27 18:38:20 +0800

    Update popup.js

--- popup.js
+++ popup.js
@@ -1,5 +1,22 @@
-let changeColor = document.getElementById("changeColor");
+for(let i=0;i<(document.querySelectorAll("div._ab8x._ab94._ab99._ab9f._ab9m._ab9o")).length;i++){
+   	console.log(document.querySelectorAll("div._ab8x._ab94._ab99._ab9f._ab9m._ab9o")[i].children[2].textContent);
+	console.log(document.querySelectorAll("div._ab8x._ab94._ab99._ab9f._ab9m._ab9o")[i].children[2].innerText.slice(-2));
+	if(document.querySelectorAll("div._ab8x._ab94._ab99._ab9f._ab9m._ab9o")[i].children[2].innerText.slice(-2)!="\\s"){
+		document.querySelectorAll("div._ab8x._ab94._ab99._ab9f._ab9m._ab9o")[i].children[2].innerHTML+="<indicator> \\s<\indicator>";
+	}
+}
 
-chrome.storage.sync.get("color", ({ color }) => {
-  changeColor.style.backgroundColor = color;
-});
+
+for(let i=0;i<(document.querySelectorAll("div._a9zs")).length;i++){
+   	console.log(document.querySelectorAll("div._a9zs")[i].children[0].textContent);
+   	console.log(document.querySelectorAll("div._a9zs")[i].children[0].innerText.slice(-2));
+	if(document.querySelectorAll("div._a9zs")[i].children[0].innerText.slice(-2)!="\\s"){
+		document.querySelectorAll("div._a9zs")[i].children[0].innerHTML+="<indicator> \\s<\indicator>";
+	}
+   
+}
+
+document.querySelector("style").innerHTML+=`
+indicator{
+    color:red;
+}`
