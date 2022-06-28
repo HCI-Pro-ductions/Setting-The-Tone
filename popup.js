9807fbff3a6912fd55aabe04ccff5f34fa648e97
Beans <106309084+Life-is-unpog@users.noreply.github.com>
2022-06-28 16:35:28 +0800

    Update popup.js

--- popup.js
+++ popup.js
@@ -1,22 +1,24 @@
-for(let i=0;i<(document.querySelectorAll("div._ab8x._ab94._ab99._ab9f._ab9m._ab9o")).length;i++){
-   	console.log(document.querySelectorAll("div._ab8x._ab94._ab99._ab9f._ab9m._ab9o")[i].children[2].textContent);
-	console.log(document.querySelectorAll("div._ab8x._ab94._ab99._ab9f._ab9m._ab9o")[i].children[2].innerText.slice(-2));
-	if(document.querySelectorAll("div._ab8x._ab94._ab99._ab9f._ab9m._ab9o")[i].children[2].innerText.slice(-2)!="\\s"){
-		document.querySelectorAll("div._ab8x._ab94._ab99._ab9f._ab9m._ab9o")[i].children[2].innerHTML+="<indicator> \\s<\indicator>";
+changeColor.addEventListener("click", =>{
+	for(let i=0;i<(document.querySelectorAll("div._ab8x._ab94._ab99._ab9f._ab9m._ab9o")).length;i++){
+		console.log(document.querySelectorAll("div._ab8x._ab94._ab99._ab9f._ab9m._ab9o")[i].children[2].textContent);
+		console.log(document.querySelectorAll("div._ab8x._ab94._ab99._ab9f._ab9m._ab9o")[i].children[2].innerText.slice(-2));
+		if(document.querySelectorAll("div._ab8x._ab94._ab99._ab9f._ab9m._ab9o")[i].children[2].innerText.slice(-2)!="\\s"){
+			document.querySelectorAll("div._ab8x._ab94._ab99._ab9f._ab9m._ab9o")[i].children[2].innerHTML+="<indicator> \\s<\indicator>";
+		}
 	}
-}
 
 
-for(let i=0;i<(document.querySelectorAll("div._a9zs")).length;i++){
-   	console.log(document.querySelectorAll("div._a9zs")[i].children[0].textContent);
-   	console.log(document.querySelectorAll("div._a9zs")[i].children[0].innerText.slice(-2));
-	if(document.querySelectorAll("div._a9zs")[i].children[0].innerText.slice(-2)!="\\s"){
-		document.querySelectorAll("div._a9zs")[i].children[0].innerHTML+="<indicator> \\s<\indicator>";
+	for(let i=0;i<(document.querySelectorAll("div._a9zs")).length;i++){
+		console.log(document.querySelectorAll("div._a9zs")[i].children[0].textContent);
+		console.log(document.querySelectorAll("div._a9zs")[i].children[0].innerText.slice(-2));
+		if(document.querySelectorAll("div._a9zs")[i].children[0].innerText.slice(-2)!="\\s"){
+			document.querySelectorAll("div._a9zs")[i].children[0].innerHTML+="<indicator> \\s<\indicator>";
+		}
+
 	}
-   
-}
 
-document.querySelector("style").innerHTML+=`
-indicator{
-    color:red;
-}`
+	document.querySelector("style").innerHTML+=`
+	indicator{
+	    color:red;
+	}`
+}
