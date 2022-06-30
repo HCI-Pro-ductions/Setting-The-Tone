42c6158075b37fb1090bffd8f39cdec7cd0caa1b
Beans <106309084+Life-is-unpog@users.noreply.github.com>
2022-06-30 11:59:54 +0800

    Update popup.js

--- popup.js
+++ popup.js
@@ -19,7 +19,7 @@ function toggleSarcasm(){
 	for(let i=0;i<(document.querySelectorAll("div._ab8x._ab94._ab99._ab9f._ab9m._ab9o")).length;i++){
 		console.log(document.querySelectorAll("div._ab8x._ab94._ab99._ab9f._ab9m._ab9o")[i].children[2].textContent);
 		console.log(document.querySelectorAll("div._ab8x._ab94._ab99._ab9f._ab9m._ab9o")[i].children[2].innerText.slice(-2));
-		if(document.querySelectorAll("div._ab8x._ab94._ab99._ab9f._ab9m._ab9o")[i].children[2].innerText.slice(-2)!="/s"){
+		if(document.querySelectorAll("div._ab8x._ab94._ab99._ab9f._ab9m._ab9o")[i].children[2].innerText.slice(-2)!="/s" && document.querySelectorAll("div._ab8x._ab94._ab99._ab9f._ab9m._ab9o")[i].children[2].innerHTML.slice(-12)!="<\indicator>"){
 			document.querySelectorAll("div._ab8x._ab94._ab99._ab9f._ab9m._ab9o")[i].children[2].innerHTML+="<indicator class='invis'> /s<\indicator>";
 		}
 		if(postIsSarcastic){ //postIsSarcastic is whatever condition we'll be using
@@ -29,7 +29,7 @@ function toggleSarcasm(){
 	for(let i=0;i<(document.querySelectorAll("div._a9zs")).length;i++){
 		console.log(document.querySelectorAll("div._a9zs")[i].children[0].textContent);
 		console.log(document.querySelectorAll("div._a9zs")[i].children[0].innerText.slice(-2));
-		if(document.querySelectorAll("div._a9zs")[i].children[0].innerText.slice(-2)!="/s"){
+		if(document.querySelectorAll("div._a9zs")[i].children[0].innerText.slice(-2)!="/s" && document.querySelectorAll("div._a9zs")[i].children[0].innerHTML.slice(-12)!="</indicator>"){
 			document.querySelectorAll("div._a9zs")[i].children[0].innerHTML+="<indicator class='invis'> /s<\indicator>";
 		}
 		if(postIsSarcastic){ //postIsSarcastic is whatever condition we'll be using
