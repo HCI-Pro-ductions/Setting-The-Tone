84a171c1b5431d937401d8fe95fd469f0c7330ed
Beans <106309084+Life-is-unpog@users.noreply.github.com>
2022-08-16 20:43:49 +0800

    Update option.js

--- option.js
+++ option.js
@@ -1,11 +1,21 @@
 // Saves options to chrome.storage
   function saveTone() {
     console.log("stuff");
+    haveFetched = false;
     var toneChoice = document.getElementById('tone').value;
     chrome.storage.sync.set({
       toneChoice: toneChoice,
     })
     console.log(toneChoice);
+    chrome.tabs.query({active: true, currentWindow: true}).then(resp=>{
+      const tabId = resp[0];
+      chrome.scripting.executeScript(
+        {
+          target: {tabId: tabId['id']},
+          func: removeIndicators,
+        },
+        () => {});
+    })
   }
   
   // Restores select box state using the preferences
@@ -22,6 +32,15 @@
       }
       document.getElementById('tone').value = items.toneChoice;
     });
+  }
+
+  function removeIndicators(){
+    console.log("removing...");
+    var element = document.getElementsByTagName("indicator"), index;
+
+    for (index = element.length - 1; index >= 0; index--) {
+      element[index].parentNode.removeChild(element[index]);
+    }
   }
 
   document.addEventListener('DOMContentLoaded', restoreTone);
