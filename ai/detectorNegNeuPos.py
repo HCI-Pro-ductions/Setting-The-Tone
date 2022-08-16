6169a438680ef4285e2957df62f61a43ef870dcb
AKEevee <191370u@student.hci.edu.sg>
2022-08-16 17:53:32 +0800

    minor improvements

--- ai/detectorNegNeuPos.py
+++ ai/detectorNegNeuPos.py
@@ -3,7 +3,7 @@ import tensorflow as tf
 
 from tensorflow.keras.preprocessing.sequence import pad_sequences
 
-max_length = 200
+max_length = 100
 trunc_type='post'
 padding_type='post'
 oov_tok = "<OOV>"
